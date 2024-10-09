bc1418c1abaca1e1987447a0edf2c27b969ebcb0
André Nascimento <Andre.Nascimento.Contractor@pepsico.com>
2024-10-09 13:39:01 -0300

    Lazy ou Eager - N+1

--- src/main/java/br/com/treinaweb/twprojects/core/repositories/EmployeeRepository.java
+++ src/main/java/br/com/treinaweb/twprojects/core/repositories/EmployeeRepository.java
@@ -1,9 +1,18 @@
 package br.com.treinaweb.twprojects.core.repositories;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.treinaweb.twprojects.core.models.Employee;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Long>{
-    
+import java.util.List;
+import java.util.Optional;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    @EntityGraph(attributePaths = "position")
+    List<Employee> findAll();
+
+    @EntityGraph(attributePaths = {"position", "address", "projects"})
+    Optional<Employee> findById(Long id);
 }
